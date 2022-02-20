87fb08bd1a65566495d76027d04c83918dd96597
Developer-Nasim <ajnasim72@gmail.coom>
2022-02-20 09:09:05 +0600

    upload

--- src/components/helper/block/index.js
+++ src/components/helper/block/index.js
@@ -3,7 +3,7 @@ import Tilt from 'react-tilt'
 import "./style.css"
 function ContentBlock({children,title,img,classNames }) {
     return (
-        <Tilt className="willMove" options={{ max : 35,perspective:1000,scale:1, }}> 
+        <Tilt className="willMove" options={{ max : 15,perspective:2500,scale:1, }}> 
             <div className={classNames ? "content-block "+classNames : "content-block"}>
                 {title ? <h5>{title}</h5> : ""}
                 { children }
