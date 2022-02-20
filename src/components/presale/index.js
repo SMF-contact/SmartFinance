092082f3f1dcc611815cca284e816d317b6537fd
Developer-Nasim <ajnasim72@gmail.coom>
2022-02-21 02:03:15 +0600

    finished

--- src/components/presale/index.js
+++ src/components/presale/index.js
@@ -65,7 +65,7 @@ function Presale() {
                             </div>
                         </ScrollAnimation>   
                     </div>
-                    <div className="col-lg-4 col-md-4 offset-lg-1">
+                    <div className="col-lg-4 col-md-5 offset-lg-1">
                         <div className="countdown">
                             <ScrollAnimation animateIn='fadeIn' animateOut='fadeOut'>   
                                 <CountDown date="Oct 25, 2023" />
